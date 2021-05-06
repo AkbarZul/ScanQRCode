6cbd343a64e9336ad84d9c36693156a5fb50991f
AkbarZul <akbar.zul73@gmail.com>
2021-05-06 20:43:26 +0700

    first commit

---
+++ src/screens/SplashScreen/index.js
@@ -0,0 +1,12 @@
+import React, {useEffect} from 'react'
+import { View, Image, StyleSheet } from 'react-native';
+
+const Splash =  ({}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Splash;
